05d59ac3aaeedd15e118bbd10592a2c6316a4f66
Chandan <chandan.uidevops@gmail.com>
2022-10-12 00:39:41 +0530

    first commit

--- src/pages/PostList.test.js
+++ src/pages/PostList.test.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { useQuery } from "react-query";
+import { useColorMode, useTheme } from "@chakra-ui/core";
 import PostList from "./PostList";
+import { BrowserRouter } from "react-router-dom";
 jest.mock("react-query");
 
 describe("PostList", () => {
@@ -12,6 +14,9 @@ describe("PostList", () => {
       error: null,
       data: null,
     });
+
+    useColorMode.mockReturnValue({ colorMode: "light" });
+    useTheme.mockReturnValue({});
     //Act
     render(<PostList isDrawerOpen={false} closeDrawer={jest.fn} />);
     //Assertion
@@ -19,8 +24,33 @@ describe("PostList", () => {
     // const text = screen.queryByText("Loading..").innerHTML;
     const text = screen.queryByTestId("loading-text");
     // expect(text).toBe("Loading..");
-    screen.
+
     expect(text).toHaveTextContent("Loading..");
   });
-  //it('When isLoading is false and data exist the render list of data')
+  it("When isLoading is false and data exist the render list of data", () => {
+    //Arrange
+    useQuery.mockReturnValue({
+      isLoading: false,
+      error: null,
+      data: {
+        data: [{ id: 1, title: "Dummy Title" }],
+      },
+    });
+
+    render(
+      <BrowserRouter>
+        <PostList isDrawerOpen={false} closeDrawer={jest.fn} />
+      </BrowserRouter>
+    );
+    const data = screen.getAllByTestId("list-item").map((li) => li.textContent);
+    //    expect(data).toEqual(['Dummy Title'])
+    expect(data).toMatchInlineSnapshot(
+      ["Dummy Title"],
+      `
+      Object {
+        "0": "Dummy Title",
+      }
+    `
+    );
+  });
 });
